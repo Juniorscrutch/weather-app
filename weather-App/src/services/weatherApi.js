07861268f515086d97bf573dd5e7100e66bf1aea
Date <gnaglodate@gmail.com>
2026-03-26 16:04:41 +0100

    Searchbar & Api

--- weather-App/src/services/weatherApi.js
+++ weather-App/src/services/weatherApi.js
@@ -1,4 +1,4 @@
-const API_KEY = "7ce95ef929fc8035a6092b2946f37955";
+const API_KEY = import.meta.env.API_KEY;
 export const getweather = async (city) => {
   const response = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`,
